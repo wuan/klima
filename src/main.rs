84c56562b7cb892b4c8600768e57c8a42567cff6
Andreas Würl <8093294-wuerla@users.noreply.gitlab.com>
2022-02-26 00:50:59 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+use clap::Parser;
+
+use configparser::ini::Ini;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(global = true, long)]
+    check: bool,
+}
+
+fn main() {
+    let args = Args::parse();
+    println!("{:?}", args);
+}
