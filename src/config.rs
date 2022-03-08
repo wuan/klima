8e70bf3c901f0cd01343fbc562dd4f6fa97b9d00
Andreas Würl <8093294-wuerla@users.noreply.gitlab.com>
2022-03-08 19:39:28 +0100

    improve error handling

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::{Error, Read};
 use std::path::Path;
 
 use configparser::ini::Ini;
@@ -15,17 +17,31 @@ enum SensorType {
     SHT1x
 }
 
-fn parse() -> Result<Config, &'static str> {
-    if !Path::new("/etc/klimalogger.conf").exists() {
-        return Result::Err("config file not found");
-    }
+fn parse() -> Result<Config, Error> {
+    let path = Path::new("/etc/klimalogger.conf");
+    let mut file = File::open(&path)?;
+
+    let mut s = String::new();
+    file.read_to_string(&mut s)?;
+
+    let config = parse_string(&s);
+
+    return Result::Ok(config);
+}
+
+fn parse_string(source: &str) -> Config {
+    let mut config = Ini::new();
+    let configString = String::from(source);
+    config.read(configString);
+
+    let value = config.getint("somesection", "someintvalue").unwrap().unwrap();
 
     let config = Config {
         client_name: "adf",
         sensors: Vec::new(),
     };
 
-    return Result::Ok(config);
+    return config;
 }
 
 #[cfg(test)]
